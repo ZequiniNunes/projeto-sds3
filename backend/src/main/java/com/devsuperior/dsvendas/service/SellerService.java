538ed52805294ab2add0efb91628a407be095950
leonardo zequini <zequini.leonardo@gmail.com>
2021-11-07 14:11:14 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/service/SellerService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SellerService.java
@@ -2,7 +2,6 @@ package com.devsuperior.dsvendas.service;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
